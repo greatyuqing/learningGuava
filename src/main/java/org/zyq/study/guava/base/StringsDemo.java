d3b50c8a461c357c5b790154c8f5e4e010aa3cb7
yuqing.zheng <greatyuqing@gmail.com>
2014-01-21 14:08:49 +0800

    finish StringsDemo

--- src/main/java/org/zyq/study/guava/base/StringsDemo.java
+++ src/main/java/org/zyq/study/guava/base/StringsDemo.java
@@ -41,5 +41,19 @@ public class StringsDemo {
         System.out.println("Strings.repeat(\"\", 0):  " + Strings.repeat("", 0));
 //        System.out.println("Strings.repeat(null, 3):  " + Strings.repeat(null, 3));   // throws NPE
 //        System.out.println("Strings.repeat(null, 0):  " + Strings.repeat(null, 0));   // throws NPE
+
+        System.out.println("----------------------------------------");
+
+        System.out.println("Strings.commonPrefix(\"abc\", \"abcd\"):  " + Strings.commonPrefix("abc", "abcd"));
+        System.out.println("Strings.commonPrefix(\"abc\", \"cba\"):  " + Strings.commonPrefix("abc", "cba"));
+        System.out.println("Strings.commonPrefix(\"abc\", \"\"):  " + Strings.commonPrefix("abc", ""));
+        // neither of the params could be null, otherwise NPE
+//        System.out.println("Strings.commonPrefix(null, null):  " + Strings.commonPrefix(null, null));
+
+        System.out.println("----------------------------------------");
+
+        System.out.println("Strings.commonSuffix(\"abc\", \"dabc\"):  " + Strings.commonSuffix("abc", "dabc"));
+        System.out.println("Strings.commonSuffix(\"abc\", \"cba\"):  " + Strings.commonSuffix("abc", "cba"));
+        System.out.println("Strings.commonSuffix(\"abc\", \"\"):  " + Strings.commonSuffix("abc", ""));
     }
 }
